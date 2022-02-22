b72e0acb3a7133d09999c9f849c22e34b56e1123
muneobu <muneobu@gmail.com>
2022-02-22 22:12:14 +0900

    :sweat:

--- New/NewJs/todo.js
+++ New/NewJs/todo.js
@@ -4,11 +4,20 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+function paintTodo(todo) {
+  console.log(todo);
+  const todoLi = document.createElement("li");
+  const todoSpan = document.createElement("span");
+  todoLi.appendChild(todoSpan);
+  todoSpan.innerHTML = todo;
+  todoList.appendChild(todoLi);
+}
+
 function handleToDoSubmit(e) {
   e.preventDefault();
   const todo = todoInput.value;
-  console.log(todoInput.value);
   todoInput.value = null;
+  paintTodo(todo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
